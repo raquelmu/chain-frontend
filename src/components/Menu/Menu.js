8a1f6023b832a7f6755c5ed3133b5c89df2fc98c
raquelmu <raquel.muniesa@gmail.com>
2020-05-30 00:36:09 +0200

    menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -10,7 +10,7 @@ export default class Menu extends Component {
                 <Link to={'/ads'}><img src="/img/home-icon.png" alt="home"/></Link>
                 <Link to={'/ads'}><img src="/img/search-icon.png" alt="search"/></Link>                <img/>
                 <Link to={'/ads/new'}><img src="/img/plus-icon.png" alt="plus"/></Link>                <img/>
-                <Link to={'/user/favorites'}><img src="/img/favorites-icon.png" alt="favorites"/></Link>
+                <Link to={'/favorites'}><img src="/img/favorites-icon.png" alt="favorites"/></Link>
                 <Link to={'/profile/:id'}><img src="/img/profile-icon.png" alt="profile"/></Link>
             </div>
         )
